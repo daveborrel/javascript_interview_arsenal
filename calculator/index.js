602981944b3c844f807cd4cea6c381c782c269c9
daveborrel <daveborrel27@gmail.com>
2025-03-08 22:06:32 -0800

    added functionality

--- calculator/index.js
+++ calculator/index.js
@@ -1,17 +1,62 @@
 // Now how do we represent a calculator in pure javaScript?
 
-class Calculator {
-    constructor(value) {
-        this.value = value
-    }
-
-    renderDisplayValue() {
-        const value = document.getElementsByClassName("numberDisplay");
-        value.textContent = this.value;
-
-        console.log(`changed value to ${this.value}`)
-    }
+const operations = {
+    "add": function(x,y) { return x + y},
+    "sub": function(x,y) { return x - y},
+    "mul": function(x,y) { return x - y},
+    "div": function(x,y) { return x - y},
 }
 
-const calculator = new Calculator(0);
-calculator.renderDisplayValue();
+class Calculator {
+    constructor() {
+        this.currentDisplayValue = ""
+        this.storedValue = ""
+        this.storedOperation = null
+        console.log(`Calculator instantiated with current value of: ${this.currentDisplayValue}`)
+    }
+
+    renderDisplayValue(newValue) {
+        console.log("Render called")
+        const value = document.getElementById("numberDisplayText");
+        this.currentDisplayValue = this.currentDisplayValue + newValue.toString()
+        value.textContent = this.currentDisplayValue
+    }
+
+    addDecimal() {
+        console.log("addDecimal called")
+        const value = document.getElementById("numberDisplayText");
+        this.currentDisplayValue = this.currentDisplayValue + "."
+        value.textContent = this.currentDisplayValue
+    }
+
+    useOperation(operation) {
+        console.log("Operation called")
+        const value = document.getElementById("numberDisplayText");
+        this.storedValue = this.currentDisplayValue
+        this.currentDisplayValue = ""
+        this.storedOperation = operation
+        value.textContent = this.currentDisplayValue
+    }
+
+    completeOperation() {
+        console.log("completeOperation called")
+        if (this.storedOperation) {
+            const value = document.getElementById("numberDisplayText");
+            const result = operations[this.storedOperation](Number(this.storedValue), Number(this.currentDisplayValue))
+            this.currentDisplayValue = ""
+            this.renderDisplayValue(result)
+        } else {
+            console.log("No operation")
+        }
+    }
+
+    clearDisplayValue() {
+        console.log("Clear called")
+        const value = document.getElementById("numberDisplayText");
+        this.currentDisplayValue = ""
+        value.textContent = this.currentDisplayValue
+    }
+    
+}
+
+const calculator = new Calculator();
